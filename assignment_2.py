e52ba9b6985360340af8315e22b8127703997aa7
VineeshaSubramanian <102983425+VineeshaSubramanian@users.noreply.github.com>
2022-04-25 16:00:02 +0530

    Update assignment_2.py

--- assignment_2.py
+++ assignment_2.py
@@ -1,30 +1,11 @@
 #assignment 2 : Fibonacci numbers
-
-# Getting Nth term
-nth_term = int(input("How many terms you need :"))
-
-# Initiation of first 2 numbers
-x=0
-y=1
-
-# Fibonacci Numbers
-if nth_term == 0:
-    print(x)
-
-elif nth_term == 0:
-    print(x)
-
-elif nth_term == 1:
-    print(y)
-
-elif nth_term > 2:
-    print(x)
-    print(y)
-    for i in range (2,nth_term):
-        z=x+y
-        x=y
-        y=z
-        print(z)
-
-else:
-    print("Enter Correct Number")
+a=0
+b=1
+limit=int(input("Enter limit of fibonacci : "))
+print(a)
+for number in range(limit):
+    c=a+b;
+    a=b
+    b=c
+    c=a
+    print(c)
