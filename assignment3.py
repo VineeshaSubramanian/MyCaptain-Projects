610a243dbd1b2a28d8c91ce2812fec7584215b9e
VineeshaSubramanian <102983425+VineeshaSubramanian@users.noreply.github.com>
2022-04-25 16:15:33 +0530

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,10 +1,18 @@
-def most_frequent(string):
-    d = dict()
-    for key in string:
-        if key not in d:
-            d[key] = 1
-        else:
-            d[key] += 1
-    return d
+def most_frequent(word):
+  word = word.upper()
+  dict = {}
+  for i in word:
+    dict[i]=0
+  for i in word:
+    dict[i]+=1
 
-print (most_frequent('mississippi'))
+  i=len(word)
+  while i>0:
+    for j in dict:
+      if dict[j]==i:
+        print(j," = ",str(i).rjust(2,'0')) 
+    i-=1
+      
+
+word = input("Please enter a string : \n")
+most_frequent(word)
